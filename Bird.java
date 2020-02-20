b7686b6206ee776c0e8a465e3cf79ab2b17030f5
Hugo Préa <hugoprea@Mac-mini-de-Coaxys-2.local>
2020-02-20 17:00:32 +0100

    exercice done

--- Bird.java
+++ Bird.java
@@ -11,16 +11,16 @@ public abstract class Bird {
     }  
   
     // getters and setters
-    public String getName() {  
-        return this.name;  
+    public String getName() {
+        return this.name;
     }  
   
-    public void setName(String name) {  
-        this.name = name;  
+    public void setName(String name) {
+        this.name = name;
     }  
   
-    public int getAge() {  
-        return this.age;  
+    public int getAge() {
+        return this.age;
     }  
   
     public void setAge(int age) {
--- Eagle.java
+++ Eagle.java
@@ -1,4 +1,4 @@
-public class Eagle extends Bird {
+public class Eagle extends Bird implements Fly {
 
     private boolean flying;
     private int altitude;
@@ -15,6 +15,46 @@ public class Eagle extends Bird {
 
     public boolean isFlying() {
         return flying;
+    }
+
+    @Override
+    public void takeOff() {
+        this.flying = true;
+        System.out.println(this.getName() + " is taking off !");
+    }
+
+    @Override
+    public int ascend(int meters) {
+        if (this.flying) {
+            this.flying = true;
+            this.altitude += meters;
+            System.out.println(this.getName() + " is ascending at " + this.altitude + " meters.");
+        }
+        return this.getAltitude();
+    }
+
+    @Override
+    public int descend(int meters) {
+        if (this.flying) {
+            this.altitude -= meters;
+            System.out.println(this.getName() + " is descending at " + this.altitude + " meters.");
+        }
+        return this.altitude;
+    }
+
+    @Override
+    public void glide() {
+        System.out.println(this.getName() + " is gliding at " + this.altitude + " meters.");
+    }
+
+
+    @Override
+    public void land() {
+        System.out.println(this.getName() + " is landing !");
+        if(altitude == 0) {
+            this.flying = false;
+            System.out.println(this.getName() + " is landed !");
+        }
     }
 
     @Override
